db5c43b51fc0f265c737571e309a575cc8957560
ryantownshend <ryan@discoverygarden.ca>
2018-03-02 08:48:23 -0400

    incrementing version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='honk',
-    version='0.4',
+    version='0.5',
     description='Tool for dumping output of HTTP requests',
     url='https://github.com/ryantownshend/honk',
     author='Ryan Townshend',
